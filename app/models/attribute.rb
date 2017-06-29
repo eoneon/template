a143b0075df252214daab96893a4ef96eb2934a3
Eon <eoneon123@gmail.com>
2017-06-28 19:20:53 -0700

    added uniquness validation to attributes; need to figure out exception handling

--- app/models/attribute.rb
+++ app/models/attribute.rb
@@ -1,9 +1,12 @@
 class Attribute < ActiveRecord::Base
   require 'csv'
-  
+
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
       Attribute.create! row.to_hash
     end
   end
+
+  #figure out best way to either ignore exception OR only add unique via ||=
+  validates_uniqueness_of :name
 end
